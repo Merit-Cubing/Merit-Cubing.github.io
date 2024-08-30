14769f627ec1b68baaf1c4d7a46f1ccf940f173d
Erti <75284271+Ertizoel@users.noreply.github.com>
2024-08-30 11:17:07 -0600

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,37 @@
+let startTime, endTime, timerInterval;
+let times = [];
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        startTime = Date.now();
+        timerInterval = setInterval(updateTimer, 10);
+    } else {
+        if (startTime) {
+            endTime = Date.now();
+            clearInterval(timerInterval);
+            let time = (endTime - startTime) / 1000;
+            times.push(time);
+            if (times.length > 5) {
+                times.shift();
+            }
+            updateStats();
+            startTime = null;
+        }
+    }
+});
+
+function updateTimer() {
+    if (startTime) {
+        let elapsedTime = (Date.now() - startTime) / 1000;
+        document.getElementById('timer').innerText = elapsedTime.toFixed(3);
+    }
+}
+
+function updateStats() {
+    if (times.length > 0) {
+        let averageTime = times.reduce((a, b) => a + b, 0) / times.length;
+        document.getElementById('ao5').innerText = averageTime.toFixed(3);
+    } else {
+        document.getElementById('ao5').innerText = 'N/A';
+    }
+}
